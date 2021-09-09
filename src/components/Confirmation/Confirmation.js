375bb80598e66d7254a3b2e0861e473358385e63
khushboo <himanshujain2597@gmail.com>
2021-09-09 16:24:08 +0530

    format cod

--- src/components/Confirmation/Confirmation.js
+++ src/components/Confirmation/Confirmation.js
@@ -1,41 +1,42 @@
 import React from "react";
-import {Button , Modal} from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 
-import { useSelector , useDispatch } from "react-redux";
-import {hideConfirmation} from "../../actions/confirmation-action";
+import { useSelector, useDispatch } from "react-redux";
+import { hideConfirmation } from "../../actions/confirmation-action";
 
-const Confirmation  = () => {
-    const confirm = useSelector((state)=>state.confirm);
-    const dispatch = useDispatch();
+const Confirmation = () => {
+  const confirm = useSelector((state) => state.confirm);
+  const dispatch = useDispatch();
 
-
-    const onClickYes = () => {
-        confirm.onClickYes(confirm.args);
-        dispatch(hideConfirmation());
+  const onClickYes = () => {
+    if (confirm.onClickYes) {
+      confirm.onClickYes(confirm.args);
     }
+    dispatch(hideConfirmation());
+  };
 
-    const onClickNo = () => {
-        dispatch(hideConfirmation());
-    }
+  const onClickNo = () => {
+    dispatch(hideConfirmation());
+  };
 
-    return (
-        <>
-        <Modal show = {confirm.show} onHide = {onClickNo}>
-            <Modal.Header>
-                <Modal.Title>{confirm.type}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-               {confirm.message}
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant = "secondary" onClick = {onClickNo}>
-                    close
-                </Button>
-                <Button variant = "primary" onClick = {onClickYes}>yes</Button>
-            </Modal.Footer>
-        </Modal>
-        </>
-    )
-}
+  return (
+    <>
+      <Modal show={confirm.show} onHide={onClickNo}>
+        <Modal.Header>
+          <Modal.Title>{confirm.type}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{confirm.message}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onClickNo}>
+            close
+          </Button>
+          <Button variant="primary" onClick={onClickYes}>
+            yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 export default Confirmation;
